84579d209d89ed04de4275624b07d57ff4e9bee3
Westwo-agency <westwoagency@gmail.com>
2025-10-24 19:59:53 -0300

    Update ProductsPage.tsx

--- components/ProductsPage.tsx
+++ components/ProductsPage.tsx
@@ -3,6 +3,7 @@ import { useAppContext } from '../context/AppContext';
 import { Card } from './ui/Card';
 import { Input } from './ui/Input';
 import { Button } from './ui/Button';
+import type { Product } from '../types';
 
 const TrashIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" /></svg>;
 const LockClosedIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" /></svg>;
@@ -18,11 +19,14 @@ export const ProductsPage: React.FC = () => {
 
   const handleAddProduct = () => {
     if (newProduct.name && newProduct.costPrice && newProduct.sellPrice) {
-      addProduct({
+      // FIX: Added the missing 'id' property to the new product object.
+      const productToAdd: Product = {
+        id: `prod_${new Date().toISOString()}`,
         name: newProduct.name,
         costPrice: parseFloat(String(newProduct.costPrice).replace(',', '.')) || 0,
         sellPrice: parseFloat(String(newProduct.sellPrice).replace(',', '.')) || 0,
-      });
+      };
+      addProduct(productToAdd);
       setNewProduct({ name: '', costPrice: '', sellPrice: '' });
     }
   };
@@ -169,6 +173,7 @@ export const ProductsPage: React.FC = () => {
                 setPasswordError('');
               }}
               icon={<LockClosedIcon />}
+              onKeyUp={(e) => e.key === 'Enter' && handleDeleteProduct()}
             />
             {passwordError && <p className="text-red-500 text-xs mt-1">{passwordError}</p>}
             <div className="flex justify-end space-x-2 mt-4">
